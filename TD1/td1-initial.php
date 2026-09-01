92ae06551c6f85f78fcffda2bc8e34a5aa9cb669
Ange Holvec <ange.holvec5@etu.univ-lorraine.fr>
2026-09-01 18:28:48 +0200

    fin du 1

--- TD1/td1-initial.php
+++ TD1/td1-initial.php
@@ -1,4 +1,15 @@
 <?php
+//Ligne pour definir le doc en tant que html
+echo "<!DOCTYPE html>\n";
+echo "<html lang=\"fr\">\n";
+
+echo "<head>\n";
+echo "    <meta charset=\"UTF-8\">\n";
+echo "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
+echo "    <title>Script php</title>\n";
+echo "</head>\n";
+
+echo "<body>\n";
 
 echo "<h1>Allo le monde ?</h1>\n" ;
 print "<h3>a l'eau, limande ?</h3> \n";
@@ -8,3 +19,8 @@ $uneVariable = "menthe";
 print $uneVariable . "<h2>Halo</h2>  \n" ;
 
 print "céfini \n";
+
+print "</body>\n";
+print "</html>\n";
+?>
+
