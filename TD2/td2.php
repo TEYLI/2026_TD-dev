2c09830ca38d5a842562d4099afb6e314e022c2d
Ange Holvec <e79363u@webetu.iutnc.univ-lorraine.fr>
2026-09-04 08:45:53 +0200

    Initialisation TD2 exo 1

---
+++ TD2/td2.php
@@ -0,0 +1,16 @@
+<?php
+//Exo 1
+function plusGrand(int $a,int $b) : int {
+    if ($a < $b){
+        return $b;
+    } else { 
+        return $a;
+    }
+}
+echo plusGrand(5,6);
+
+//Exo 2
+
+
+?>
+
