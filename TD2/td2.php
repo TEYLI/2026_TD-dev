278207f37e26bb66dd463edffbe1143da1e6ff43
Ange Holvec <e79363u@webetu.iutnc.univ-lorraine.fr>
2026-09-04 09:02:04 +0200

    Exo2

--- TD2/td2.php
+++ TD2/td2.php
@@ -10,7 +10,18 @@ function plusGrand(int $a,int $b) : int {
 echo plusGrand(5,6);
 
 //Exo 2
+function divisible(int $max,int $div){
+    for ($i = $max; $i >= 0; $i--) {
+        echo $i;
+        if ($i % $div === 0) {
+            echo " (divisible par $div)";
+        }
+        echo "\n"; 
+    }
+}
 
+divisible(10,12);
 
+//Exo
 ?>
 
